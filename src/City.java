ab4692d27dfb0f86e492f71870698b1bbfedd350
Tuomas <tuomasjar77@gmail.com>
2022-01-23 18:42:20 +0200

    Added city and map classes

--- src/City.java
+++ src/City.java
@@ -1,4 +1,21 @@
+import java.util.HashMap;
 
 public class City {
+	HashMap<String, Integer> destinations;
+	private String name;
 
+	public City(String name) {
+		this.name = name;
+	}
+
+	public void addDestination(String destination, int length) throws AlreadyAddedError {
+		if (destinations.containsKey(destination)) {
+			throw new AlreadyAddedError();
+		}
+		destinations.put(destination, length);
+	}
+
+	public String getName() {
+		return name;
+	}
 }
--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
 
 public class Map {
+	private ArrayList<City> cities;
+	private String continent;
+
+	public Map(String continent) {
+		this.continent = continent;
+	}
 
 }
